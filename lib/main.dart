15e23a2b4f3520d60e11af9eb9f422034486a9e4
aurelie <aurelie.audemar@orange.fr>
2023-04-25 15:03:02 +0200

    Question 3 Etape 4 TP fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -30,43 +32,100 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final List<int> _items = [];
+
+  bool _isPrime(int number) {
+    if (number <= 1) {
+      return false;
+    }
+
+    for (int i = 2; i <= sqrt(number); i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+
+  }
+
+    Widget _getItemImage(int value) {
+    if (_isPrime(value)) {
+      return Image.asset('assets/images/ananas.png',
+      width: 48,
+      height: 48,);
+    } else if (value.isEven) {
+      return Image.asset('assets/images/pomme.png',
+      width: 48,
+      height: 48,);
+    } else {
+      return Image.asset('assets/images/poire.png',
+      width: 48,
+      height: 48,);
+    }
+  }
+
+  String _getItemType(int value) {
+    if (_isPrime(value)) {
+      return 'nombre premier';
+    } else if (value.isEven) {
+      return 'pair';
+    } else {
+      return 'impair';
+    }
+  }
+
+  Color _getItemColor(int value) {
+    if (_isPrime(value)) {
+      return Colors.indigo;
+    } else {
+      return Colors.cyan;
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
+      _items.add(_counter);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text('$_counter (${_getItemType(_counter)})'),
       ),
       body: Center(
+        child: ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            final value = _items[index];
 
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            return Container(
+              color: _getItemColor(value),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: _getItemImage(value),
+                  ),
+                  Text(
+                    '$value',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
